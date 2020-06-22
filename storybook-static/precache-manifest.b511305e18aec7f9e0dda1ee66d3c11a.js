32bfc2053555cde1109ce29db845b2e3ee613c50
Quoc <tqviet.0503@gmail.com>
2020-06-22 16:24:46 +0700

    deploy firebase

---
+++ storybook-static/precache-manifest.b511305e18aec7f9e0dda1ee66d3c11a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11041b921d08b1ac6b5235add00deda6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c1b2138f85d719b26ab3.bundle.js"
+  },
+  {
+    "url": "runtime~main.c1b2138f85d719b26ab3.bundle.js"
+  },
+  {
+    "revision": "74b6f97487a405146087",
+    "url": "static/css/main.f206a4b1.chunk.css"
+  },
+  {
+    "revision": "0be0bad42224844534d6",
+    "url": "static/css/vendors~main.829c9cb5.chunk.css"
+  },
+  {
+    "url": "vendors~main.c1b2138f85d719b26ab3.bundle.js"
+  },
+  {
+    "url": "vendors~main.c1b2138f85d719b26ab3.bundle.js.LICENSE.txt"
+  }
+]);
